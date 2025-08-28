8f14cff64dd76406dc68b770b87b131397368a2a
mumtaza <mumtazmohamed453@gmail.com>
2025-08-28 22:30:59 +0300

    python

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    
+    fib = [0]
+    if length > 1:
+        fib.append(1)
+        for i in range(2, length):
+            fib.append(fib[i-1] + fib[i-2])
+    
+    print(fib)
